1fe6cda4fd7173480d762aa963908aa50f232d1b
Valeria <valleriademelo@gmail.com>
2024-10-07 11:30:51 -0300

    Added AuthContext to manage authentication state

---
+++ src/context/AuthContext.js
@@ -0,0 +1,30 @@
+// src/context/AuthContext.js
+import React, { createContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    return JSON.parse(localStorage.getItem('loggedInUser'));
+  });
+  const navigate = useNavigate();
+
+  const login = (userData) => {
+    setUser(userData);
+    localStorage.setItem('loggedInUser', JSON.stringify(userData));
+    navigate('/contacts');
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem('loggedInUser');
+    navigate('/login');
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
